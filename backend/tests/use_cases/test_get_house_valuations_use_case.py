3a62b5a6a44da259ffb0ee66dc891d38ae980093
Dan Tche <dtcheche@gmail.com>
2024-12-26 09:51:55 -0300

    Add test for provider exception

--- backend/tests/use_cases/test_get_house_valuations_use_case.py
+++ backend/tests/use_cases/test_get_house_valuations_use_case.py
@@ -69,3 +69,36 @@ class TestGetHouseValuationsUseCase:
             septic_system=False,
             sale_price=204716,
         )
+
+    @patch.object(BasePropertyAdapter, '_get_data_from_provider')
+    def test_get_house_valuations_provider_exception(
+        self,
+        mock_get_data_from_provider,
+        mock_provider_1_data,
+    ):
+        def mock_side_effect(address, provider_name):
+            if provider_name == 'Provider 1':
+                return  mock_provider_1_data
+            elif provider_name == 'Provider 2':
+                Exception("Provider 2 error")
+            return {}
+
+        mock_get_data_from_provider.side_effect = mock_side_effect
+        result = GetHouseValuationUseCase().execute("1180 6th Avenue, New York, NY 10036")
+
+        assert len(result) == 1
+        assert isinstance(result[0], PropertySchema)
+        assert result[0] == PropertySchema(
+            id="1180-6Th-Avenue-New-York-NY-10036",
+            provider="Provider 1",
+            address="1180 6Th Avenue New York NY 10036",
+            square_footage=3968,
+            lot_size_acres=0.14,
+            year_built=1946,
+            property_type="Single Family",
+            bedrooms=1,
+            bathrooms=2,
+            room_count=9,
+            septic_system=False,
+            sale_price=204716,
+        )
